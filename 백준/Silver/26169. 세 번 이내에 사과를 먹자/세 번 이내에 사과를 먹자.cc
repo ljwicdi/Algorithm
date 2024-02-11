327c7eed68f59413003f234716807f9961822124
Lee Jae Woong <114080852+ljwicdi@users.noreply.github.com>
2024-02-11 12:32:08 +0900

    [Silver III] Title: 세 번 이내에 사과를 먹자, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+vector<int> v[5];
+int result = 0;
+void dfs(int x, int y, vector<bool> visited[], int distance, int appleCnt) {
+    vector<bool> vv[5];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            vv[i].push_back(visited[i][j]);
+        }
+    }
+    vv[x][y] = true;
+    int apple;
+    
+    if (v[x][y] == 1) {
+        apple = appleCnt + 1;
+    } else {
+        apple = appleCnt;
+    }
+    
+    if (apple >= 2) {
+        if (distance <= 3) {
+            result = 1;
+            return;
+        }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5 || v[nx][ny] == -1) {
+            continue;
+        } 
+        
+        if (vv[nx][ny] == false) {
+            dfs(nx, ny, vv, distance + 1, apple);
+        }
+    }
+}
+
+int main()
+{
+    
+    vector<bool> visited[5];
+    int data;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> data;
+            v[i].push_back(data);
+            visited[i].push_back(false);
+        }
+    }
+    
+    int r, c;
+    cin >> r >> c;
+    
+    dfs(r, c, visited, 0, 0);
+    
+    cout << result;
+    return 0;
+}
