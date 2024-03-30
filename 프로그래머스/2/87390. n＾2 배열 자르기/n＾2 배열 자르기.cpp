0f965a5f19e0b487fd0c89e87cd5b995ae3d36fc
Lee Jae Woong <114080852+ljwicdi@users.noreply.github.com>
2024-03-30 14:59:18 +0900

    [level 2] Title: n^2 배열 자르기, Time: 26.27 ms, Memory: 20.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    for (long long i = left; i <= right; i++) {
+        int row = i / n;
+        int col = i % n;
+        if (col < row) {
+            answer.push_back(row + 1);
+        } else {
+            answer.push_back(col + 1);
+        }
+    }
+    return answer;
+}
