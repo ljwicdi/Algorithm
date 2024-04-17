d097584dbc56f8a6bef24311ff3edfcde1fc322e
Lee Jae Woong <114080852+ljwicdi@users.noreply.github.com>
2024-04-17 10:31:07 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -7,15 +7,19 @@ int main() {
     
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0); 
     
-    
     int L;
     cin >> L;
     string str;
     cin >> str;
     
-    long long sum = 0;
+    unsigned long sum = 0;
     for (int i = 0; i < L; i++) {
-        sum = sum + (str[i] - 96) * pow(31, i);
+        unsigned long gob = 1;
+        for (int j = 0; j < i; j++) {   
+            gob = gob * 31 % 1234567891;
+        }
+        sum = sum + (str[i] - 96) * gob % 1234567891;
+        
     }
     cout << sum % 1234567891;
     return 0;
