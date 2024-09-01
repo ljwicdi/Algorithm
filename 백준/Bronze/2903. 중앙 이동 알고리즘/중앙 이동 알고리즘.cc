65d650e6d73b895f04b37e93d506ab656235ee80
Lee Jae Woong <114080852+ljwicdi@users.noreply.github.com>
2024-09-01 15:08:00 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n;
+	int x = 2;
+
+	cin >> n;
+
+	while(n--) {
+		x += (x - 1);
+	}
+
+	x *= x;
+
+	cout << x << endl;
+
+	return 0;
+}
