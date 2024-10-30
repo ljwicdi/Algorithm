d068b009a3e5e64619a356e0c2278e87b51e647d
Lee Jae Woong <114080852+ljwicdi@users.noreply.github.com>
2024-10-30 17:39:25 +0900

    [Silver V] Title: 피보나치 수 4, Time: 140 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string.h>
+#include <limits.h>
+#include <vector>
+#include <math.h>
+#include <stack>
+#include <bitset>
+#include <string>
+typedef long long ll;
+using namespace std;
+string add(string a, string b) {
+    int sum = 0;
+    string res;
+    while (!a.empty() || !b.empty() || sum) {
+        if (!a.empty()) sum += a.back() - '0', a.pop_back();
+        if (!b.empty()) sum += b.back() - '0', b.pop_back();
+        res.push_back((sum % 10) + '0');
+        sum /= 10;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int n;
+    cin >> n;
+    string first = "0", second = "1", res = "1";
+    for (int i = 1;i < n;i++) {
+        res = add(first, second);
+        first = second;
+        second = res;
+    }
+    if (n == 0) cout << "0"<<'\n';
+    else cout << res<<'\n';
+   
+	return 0;
+}
